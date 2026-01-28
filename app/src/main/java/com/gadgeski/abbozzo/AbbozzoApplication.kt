51774734557d766abd8273baa113b56880b06edf
gadgeski <xtwh.ce5bil@gmail.com>
2026-01-28 09:41:38 +0900

    refactor: remove legacy Abbozzo components

--- app/src/main/java/com/gadgeski/abbozzo/AbbozzoApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.gadgeski.abbozzo
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class AbbozzoApplication : Application()
--- app/src/main/java/com/gadgeski/abbozzo/data/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.gadgeski.abbozzo.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [LogEntry::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun logDao(): LogDao
-}
